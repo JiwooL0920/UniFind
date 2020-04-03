7b4a622c845ae8e5c63653026ffee760dd1c8ea8
JiwooL0920 <leej229@mcmaster.ca>
2020-04-03 15:37:29 -0400

    edit university class

--- UniFind/app/src/main/java/com/example/unifind/University.java
+++ UniFind/app/src/main/java/com/example/unifind/University.java
@@ -1,4 +1,46 @@
 package com.example.unifind;
+import java.util.ArrayList;
 
 public class University {
+    private String name;
+    private ArrayList<Program> programs;
+    private int ranking;
+
+    //Constructor
+    public University(String name) {
+        this.name = name;
+        this.programs = new ArrayList<Program>();
+        this.ranking = 0; //null
+    }
+
+    //Getter for name
+    public String name() { return this.name; }
+
+    //Getter for programs
+    public ArrayList<Program> programs() { return this.programs; }
+
+    //Getter for ranking
+    public int ranking() { return this.ranking; }
+
+    //Setter for ranking
+    public void setRanking(int n) { this.ranking = n; }
+
+    //Setter for programs (add a program to arraylist)
+    public void addProgram(Program p) { this.programs.add(p); }
+
+    //return true if program exists in university
+    public boolean hasProgram(String name) {
+        for (Program p : this.programs) {
+            if (p.name().equals(name)) return true;
+        } return false;
+    }
+
+    //return program in the arraylist given name
+    public Program getProgram(String name) {
+        if (!hasProgram(name)) return null;
+        for (Program p : this.programs) {
+            if (p.name().equals(name)) return p;
+        }
+    }
+
 }
